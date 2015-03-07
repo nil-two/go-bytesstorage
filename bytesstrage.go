95219442deb5ec197bde16b711d1f38f83fb8545
wara <kusabashira227@gmail.com>
2015-03-08 01:49:47 +0900

    Initial commit

---
+++ bytesstrage.go
@@ -0,0 +1,24 @@
+package bs
+
+// BytesStorage is a storage to hold the bytes sent over the write of io.writer.
+type BytesStorage struct {
+	buf []byte
+}
+
+// Write add the bytes in storage.
+func (w *BytesStorage) Write(p []byte) (int, error) {
+	w.buf = append(w.buf, p...)
+	return len(p), nil
+}
+
+// Load returns contents of the storage.
+func (w *BytesStorage) Load() []byte {
+	return w.buf
+}
+
+// NewBytesStorage returns a new BytesStorage
+func NewBytesStorage() *BytesStorage {
+	return &BytesStorage{
+		buf: make([]byte, 0),
+	}
+}
